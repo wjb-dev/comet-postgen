add1224052be9d45209d91e8ba3d84041036db18
burkswill2 <burks.113@osu.edu>
2025-06-27 22:21:24 -0400

    release: v0.2.49 Bug Fix reorging imports and clean up

--- haraka/__init__.py
+++ haraka/__init__.py
@@ -2,4 +2,4 @@
 from haraka.post_gen.runner import main
 from haraka.post_gen.config import PostGenConfig
 from haraka.PyFast.Runtime import Lifecycle
-__all__ = ["main", "PostGenConfig", "PyFast"]
+__all__ = ["main", "PostGenConfig", "Lifecycle"]
